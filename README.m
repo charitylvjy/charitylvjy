e932f90002c58c940044d008d44581bdba7c183b
Charlie <139638809+charitylvjy@users.noreply.github.com>
2023-07-15 16:19:41 +0100

    Idek what this is
    Read the title

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi I’m @charitylvjy.
+- 👀 I’m interested in gaming, drawing, making content and friends, etc.
+- 🌱 My goal is to be a very big streamer.
+- 💞️ I’m looking to collaborate with streamers i look up to!
+- 📫 How to reach me: contact me through my dms on wattpad and/or twitch 
+
+<!---
+charitylvjy/charitylvjy is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
